623f4b2abb15f20d16fd2eb0f9583dda8fdcf438
Rinkal Dhankariya <rinkal.dhankariya@iflair.com>
2025-01-24 16:52:24 +0530

    file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingPage from './pages/SettingPage'
+import ProfilePage from './pages/ProfilePage'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
 
 const App = () => {
+  const {authUser , checkAuth , isCheckingAuth} = useAuthStore();
+
+  useEffect(() =>{
+    checkAuth();
+  } , [checkAuth])
+
+  console.log({authUser} , "authUser");
+
+  if(isCheckingAuth && !authUser) return(
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin' />
+    </div>
+  )
   return (
-   <>
-    <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>  
-   </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path ="/" element = {<HomePage/>} />
+        <Route path ="/signup" element = {<SignUpPage/>} />
+        <Route path ="/login" element = {<LoginPage/>} />
+        <Route path ="/setting" element = {<SettingPage/>} />
+        <Route path ="/profile" element = {<ProfilePage/>} />
+      </Routes>
+    </div>
   )
 }
 
